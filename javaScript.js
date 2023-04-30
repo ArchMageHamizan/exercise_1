30148f0973022edb055c3e69d5e37202b13b6500
HamizanYusni <hamizanyus000@gmail.com>
2023-04-30 18:10:25 +0800

    re-update

--- javaScript.js
+++ javaScript.js
@@ -1,5 +1,8 @@
 let playerChoice;
 let computerChoice;
+let playerScore=0;
+let computerScore=0;
+let counter=0;
 
 
 function getComputerChoice(){
@@ -7,6 +10,24 @@ function getComputerChoice(){
     const computer=Math.floor(Math.random()*choices.length);
     return choices[computer];
 }
+
+function game(counter,playerChoice,computerChoice){
+    
+
+    
+    
+    
+
+    const result=playRound(playerChoice,computerChoice);
+    
+    if(result=="You win!"){playerScore++;}
+    else if(result=="You Lose!"){computerScore++;}
+    
+    document.getElementById("roundResult").textContent=playerScore+"-"+computerScore;
+    
+    
+}
+
 function getSelectValue(){
     var playerSelection = document.getElementById("dropdown");
     var playerOption= playerSelection.value;
@@ -14,16 +35,40 @@ function getSelectValue(){
 
     output.textContent=playerOption;
     document.getElementById("p").textContent = playerOption;
-    playerChoice=playerOption;
 
+    playerChoice=playerOption;
     computerChoice=getComputerChoice();
+
     document.getElementById("c").innerHTML=computerChoice;
     document.getElementById("computer").innerHTML=computerChoice;
 
     var result=playRound(playerChoice,computerChoice);
     document.getElementById("result").textContent=result;
 
+    counter++;
+    document.getElementById("roundValue").textContent=counter;
+    game(counter,playerChoice,computerChoice);
+
+    if(counter==5 && playerScore>computerScore){
+        document.getElementById("finalResult").textContent="You won!";
+
+    }
+    if(counter==5 && playerScore<computerChoice){
+        document.getElementById("finalResult").textContent="You lose!";
+
+    }
+
+   
+    if(counter>5){
+        counter=0;
+        playerScore=0;
+        computerScore=0;
+        document.getElementById("finalResult").textContent=" ";
+
+    }
     return playerOption;
+
+    
     
     
 }
@@ -45,3 +90,4 @@ function playRound(player,computer){
         return "You Lose!";
     
 }
+
